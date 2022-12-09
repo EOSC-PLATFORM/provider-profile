a8911c685985003ff6e2472e03425e827e148d19
sab <csabeel@gmail.com>
2022-12-09 09:03:38 +0100

    a method to fetch vocabulary, has been implemented.

---
+++ tools/fetchVocabulary.py
@@ -0,0 +1,13 @@
+import requests
+
+#TODO: add method parameter so that vocabulary can be fetcthed by type
+def getVocabularies():
+    response_API = requests.get('https://api.eosc-portal.eu/vocabulary/byType/SCIENTIFIC_DOMAIN')
+    # print(response_API.status_code)
+    data = response_API.json()
+    scientific_subdomain = []
+    for jsonObject in data:
+        scientific_subdomain.append(jsonObject['name'])
+
+    # print(scientific_subdomain)
+    return scientific_subdomain
