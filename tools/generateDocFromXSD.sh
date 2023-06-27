d7278d0fc00d2c6aadef4fd0147c7848ec8ae9c8
Andreas Czerniak <andreas.czerniak@uni-bielefeld.de>
2023-06-27 12:01:32 +0200

    preparation of v4.1-rc

--- tools/generateDocFromXSD.sh
+++ tools/generateDocFromXSD.sh
@@ -6,5 +6,4 @@
 #
 # 2022 , Bielefeld University Library: Andreas Czerniak, Sabeel Shah
 
-xmlstarlet tr xslt/selectDocumentationsForComplexTypeProvider.xsl ../schemas/schema1.xsd > ../docs/elementsProvider.rst 2>> logs/exceptionTRprovider.log
-
+xmlstarlet tr xslt/selectDocumentationsForComplexType.xsl ../schemas/schema1.xsd > ../docs/elements.rst 2>> exceptionTR.log
