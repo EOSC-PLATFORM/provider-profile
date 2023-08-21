85a1724012c4f0e738a2efd67faee7574718dec5
Andreas Czerniak <andreas.czerniak@uni-bielefeld.de>
2023-08-21 10:08:45 +0200

    cleanup

--- tools/extractHLEfromProviders.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-#
-#
-# 2022 , Andreas Czerniak
-
-cd $1
-
-xmlstarlet sel -t -m '//hostingLegalEntity' -v . -n *.xml | sort -u | grep -v '^$' | sed 's#provider_hosting_legal_entity-##'
